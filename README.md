df74d003006140b10ec51e7255a56729cb0fb640
Jingmin Zhang <jingmin1987@gmail.com>
2023-07-02 22:26:43 -0400

    added more mock backend logics and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Simple Chatbox UI
-A component needed for a bigger project
-Language: HTML, CSS and Python (flask)
-To be updated once it's done
+This is part of a project that we intended to build as a demo, aiming to showcase a possibility to combine 
+optimization and LLM to improve customer's experience.
+
+The overall flow is
+1. Get user input and extract information
+2. Keep prompting for additional information if needed
+3. Run backend algorithms
+4. Return response to the user
+
+## Mock-up UI
+![](mock_ui.png)
--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
-from random import randint
 import time
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__, static_folder='static')
+FULL_RESPONSE = {}
 
 
 @app.route("/")
@@ -13,12 +14,38 @@ def main():
 @app.route("/get")
 def get_llm_response():
     user_input = request.args.get('user_input')
-    response = run_llm(user_input)
-    time.sleep(randint(0, 1))
-    return response
+    user_response = parse_user_input(user_input)
+    FULL_RESPONSE.update(user_response)
+
+    needed_keys = ['age', 'income']  # To be updated
+    missing_input = find_missing_input(needed_keys, FULL_RESPONSE)
+    time.sleep(0.25)
+    if missing_input:
+        return f'Please provide me your {missing_input[0]}'
+    else:
+        return run_optimization(FULL_RESPONSE)
 
 
-def run_llm(user_input):
+def parse_user_input(user_input) -> dict:
+    # Run LLM to parse
+    return dict()
+
+
+def find_missing_input(needed_keys, response: dict) -> list:
+    """
+    Finds what information is missing from all the responses so far
+    :param needed_keys: needed keys
+    :param response: a given response dict
+    :return:
+    """
+
+    missing_keys = [k for k in needed_keys if k not in response]
+
+    return missing_keys
+
+
+def run_optimization(response: dict):
+    # optimization + LLM
     return 'You got me; backend is not implemented yet!'
 
 
